96ece462257e8a30d8c3e056699bd7f170d474b2
Mathieu <mathieu.letyrant@gmail.com>
2015-05-02 16:39:17 +0200

    Back - Prepare AionConfig

---
+++ resources/lang/en/aion.php
@@ -0,0 +1,57 @@
+<?php
+
+return [
+
+    'class_name' => [
+        'WARRIOR'		=> "Warrior",
+        'GLADIATOR'		=> "Gladiator",
+        'TEMPLAR'		=> "Templar",
+        'SCOUT'			=> "Scout",
+        'ASSASSIN'		=> "Assassin",
+        'RANGER'		=> "Ranger",
+        'MAGE'			=> "Mage",
+        'SORCERER'		=> "Sorcerer",
+        'SPIRIT_MASTER'	=> "Spirit Master",
+        'PRIEST'		=> "Priest",
+        'CLERIC'		=> "Cleric",
+        'CHANTER'		=> "Chanter",
+        'ENGINEER'		=> "Enginner",
+        'GUNNER'		=> "Gunner",
+        'ARTIST'		=> "Artist",
+        'BARD'			=> "Bard",
+        'RIDER'			=> "Rider"
+    ],
+
+    'race_name' => [
+        'ELYOS'		=> "Elyos",
+        'ASMODIANS'	=> "Asmodian",
+        'BALAUR'	=> "Balaur"
+    ],
+
+    'gender_name' => [
+        'MALE'		=> "Male",
+        'FEMALE'	=> "Female"
+    ],
+
+    'rank_name' => [
+        1	=> "Soldat, rang 9",
+        2	=> "Soldat, rang 8",
+        3	=> "Soldat, rang 7",
+        4	=> "Soldat, rang 6",
+        5	=> "Soldat, rang 5",
+        6	=> "Soldat, rang 4",
+        7	=> "Soldat, rang 3",
+        8	=> "Soldat, rang 2",
+        9	=> "Soldat, rang 1",
+        10	=> "Officier 1 étoiles",
+        11	=> "Officier 2 étoiles",
+        12	=> "Officier 3 étoiles",
+        13	=> "Officier 4 étoiles",
+        14	=> "Officier 5 étoiles",
+        15	=> "Général d'armée",
+        16	=> "Grand général d'armée",
+        17	=> "Commandant d'armée",
+        18	=> "Gouverneur d'armée",
+    ]
+
+];
---
+++ resources/lang/fr/aion.php
@@ -0,0 +1,57 @@
+<?php
+
+return [
+
+    'class_name' => [
+        'WARRIOR'		=> "Guerrier",
+        'GLADIATOR'		=> "Gladiateur",
+        'TEMPLAR'		=> "Templier",
+        'SCOUT'			=> "Eclaireur",
+        'ASSASSIN'		=> "Assassin",
+        'RANGER'		=> "Rodeur",
+        'MAGE'			=> "Mage",
+        'SORCERER'		=> "Sorcier",
+        'SPIRIT_MASTER'	=> "Spiritualiste",
+        'PRIEST'		=> "Pretre",
+        'CLERIC'		=> "Clerc",
+        'CHANTER'		=> "Aède",
+        'ENGINEER'		=> "Ingénieur",
+        'GUNNER'		=> "Pistolero",
+        'ARTIST'		=> "Artiste",
+        'BARD'			=> "Barde",
+        'RIDER'			=> "Cavalier"
+    ],
+
+    'race_name' => [
+        'ELYOS'		=> "Elyseen",
+        'ASMODIANS'	=> "Asmodien",
+        'BALAUR'	=> "Balaur"
+    ],
+
+    'gender_name' => [
+        'MALE'		=> "Homme",
+        'FEMALE'	=> "Femme"
+    ],
+
+    'rank_name' => [
+        1	=> "Soldat, rang 9",
+        2	=> "Soldat, rang 8",
+        3	=> "Soldat, rang 7",
+        4	=> "Soldat, rang 6",
+        5	=> "Soldat, rang 5",
+        6	=> "Soldat, rang 4",
+        7	=> "Soldat, rang 3",
+        8	=> "Soldat, rang 2",
+        9	=> "Soldat, rang 1",
+        10	=> "Officier 1 étoiles",
+        11	=> "Officier 2 étoiles",
+        12	=> "Officier 3 étoiles",
+        13	=> "Officier 4 étoiles",
+        14	=> "Officier 5 étoiles",
+        15	=> "Général d'armée",
+        16	=> "Grand général d'armée",
+        17	=> "Commandant d'armée",
+        18	=> "Gouverneur d'armée",
+    ]
+
+];
