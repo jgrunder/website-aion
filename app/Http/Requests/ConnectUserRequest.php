f4208695ca49cf85e1210bda66bedc56aa5e6a15
Mathieu <mathieu.letyrant@gmail.com>
2015-05-02 10:27:46 +0200

    Back - Add ConnectUserRequest

---
+++ app/Http/Requests/ConnectUserRequest.php
@@ -0,0 +1,40 @@
+<?php namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+use App\Models\AccountData;
+
+class ConnectUserRequest extends Request {
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+        $user = AccountData::acivated()
+                    ->where('name', $this->username)
+                    ->where('password', base64_encode(sha1($this->password, true)))
+                    ->first();
+
+        if($user !== null){
+            return true;
+        } else {
+            return false;
+        }
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [
+			'username' => 'required|unique',
+            'password' => 'required'
+		];
+	}
+
+}
--- app/Models/AccountData.php
+++ app/Models/AccountData.php
@@ -9,4 +9,12 @@ class AccountData extends Model {
     protected $table        = 'account_data';
     protected $connection   = 'loginserver';
 
+    /**
+     * Add in Scope function for select account acivated
+     */
+    public function scopeAcivated($query)
+    {
+        return $query->where('activated', 1);
+    }
+
 }
