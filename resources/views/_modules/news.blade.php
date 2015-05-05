d18f5b1e57b852949043a3c2829e651cc59b3871
Mathieu <mathieu.letyrant@gmail.com>
2015-05-05 12:24:34 +0200

    Back - Add str limit in home page for news

--- resources/views/_modules/news.blade.php
+++ resources/views/_modules/news.blade.php
@@ -1,7 +1,7 @@
 <div class="article">
     @foreach($news as $article)
         <h2>{{$article->title}}</h2>
-        <p>{{$article->text}}</p>
+        <p>{{str_limit($article->text, $limit = 300, $end = '...')}}</p>
         <span>Published by {{$article->creator->pseudo}} il y a {{Carbon::parse($article->updated_at)->diffForHumans()}}</span>
     @endforeach
 </div>
