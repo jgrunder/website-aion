5b9d5ed8ea66d1582b95fe93ee4d99a897386988
Mathieu <mathieu.letyrant@gmail.com>
2015-07-01 22:04:19 +0200

    SEO - Improvement

--- config/seotools.php
+++ config/seotools.php
@@ -6,7 +6,7 @@ return [
          * The default configurations to be used by the meta generator.
          */
         'defaults'       => [
-            'title'       => "RealAion", // set false to total remove
+            'title'       => "RealAion 4.7", // set false to total remove
             'description' => "Serveur privé Aion fun et PVP. En version 4.7", // set false to total remove
             'separator'   => ' - ',
             'keywords'    => ['aion', 'serveur', 'fun', 'pvp', 'serveur privee', 'serveur aion', 'gratuit'],
@@ -16,7 +16,7 @@ return [
          * Webmaster tags are always added.
          */
         'webmaster_tags' => [
-            'google'    => null,
+            'google'    => 'WdYf2ZSXWuYDHBwR22rlUX-8THYZoF19pSGho3jyrzQ',
             'bing'      => null,
             'alexa'     => null,
             'pinterest' => null,
@@ -28,11 +28,11 @@ return [
          * The default configurations to be used by the opengraph generator.
          */
         'defaults' => [
-            'title'       => 'RealAion', // set false to total remove
+            'title'       => 'RealAion 4.7', // set false to total remove
             'description' => 'Serveur privé Aion fun et PVP. En version 4.7', // set false to total remove
             'url'         => 'http://realaion.com',
-            'type'        => false,
-            'site_name'   => false,
+            'type'        => 'website',
+            'site_name'   => 'RealAion 4.7',
             'images'      => [],
         ]
     ],
