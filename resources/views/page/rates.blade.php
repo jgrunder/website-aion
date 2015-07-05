9807c9bd7b782220bd0e38d6fe681d8efd23e3d5
Mathieu <mathieu.letyrant@gmail.com>
2015-07-05 10:46:16 +0200

    Front - Page Rates & Team

--- resources/views/page/rates.blade.php
+++ resources/views/page/rates.blade.php
@@ -5,7 +5,8 @@
   <div class="container_single_top">
     <h1>Rates du serveur</h1>
   </div>
-  <div class="container_single_body">
+  <div class="container_single_body center">
+      <img src="/images/rates.png" alt="Rates du serveur RealAion"/>
   </div>
 </div>
 @stop
