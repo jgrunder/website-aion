a9ac2f8e3fe284ede731f1ecd0f7a0674a1ded18
Mathieu <mathieu.letyrant@gmail.com>
2016-03-03 22:47:18 +0100

    test

--- app/Http/Middleware/HasVote.php
+++ app/Http/Middleware/HasVote.php
@@ -21,12 +21,15 @@ class HasVote {
     {
         if(Session::has('connected')){
 
+            dd($request->headers->get('referer'));
+
             $accountId      = Session::get('user.id');
             $votesInConfig  = Config ::get('aion.vote.links');
             $realPerVote    = (!Config::get('aion.vote.boost')) ? Config::get('aion.vote.real_per_vote') : Config::get('aion.vote.real_per_vote') + 50;
             $referer        = $request->headers->get('referer');
 
             foreach ($votesInConfig as $key => $value) {
+
                 if($referer === $value['referer']){
                     $vote = AccountVote::where('account_id', $accountId)->where('add', 0)->where('site', $key)->first();
 
