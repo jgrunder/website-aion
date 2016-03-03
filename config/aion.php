fcfadbef0fcfe7e8a2cc77e3ce073147f80bbf65
Mathieu <mathieu.letyrant@gmail.com>
2016-03-03 22:11:52 +0100

    Add referer for RPG & Gowonda

--- config/aion.php
+++ config/aion.php
@@ -95,12 +95,12 @@ return [
       [
         'name'               => 'RPG',
         'link'               => 'http://www.rpg-paradize.com/?page=vote&vote=99942',
-        'referer'            => null
+        'referer'            => 'http://www.rpg-paradize.com/out.php?num=99942'
       ],
       [
         'name'               => 'Gowonda',
         'link'               => 'http://www.gowonda.com/vote.php?server_id=5635',
-        'referer'            => null
+        'referer'            => 'http://www.gowonda.com/out.php?server_id=5635'
       ],
     ]
   ],
