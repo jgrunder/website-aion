897b9c27efbfc5a9c74925ae9dab3d9277c41b43
Mathieu <mathieu.letyrant@gmail.com>
2016-08-27 11:34:28 +0200

    Translate pages on BackOffice

--- resources/views/admin/page.blade.php
+++ resources/views/admin/page.blade.php
@@ -9,12 +9,12 @@
             <div class="col-md-12">
                 {!! Form::open() !!}
 
-                <h2>Français :</h2>
+                <h2>French :</h2>
                 <div class="form-group">
                     {!! Form::textarea('fr', $page->fr, ['class' => 'form-control ckeditor', 'required' => 'required', 'rows' => 10, 'cols' => 40]) !!}
                 </div>
 
-                <h2>Anglais :</h2>
+                <h2>English :</h2>
                 <div class="form-group">
                     {!! Form::textarea('en', $page->en, ['class' => 'form-control ckeditor', 'required' => 'required', 'rows' => 10, 'cols' => 40]) !!}
                 </div>
