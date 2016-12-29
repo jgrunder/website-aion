83d5315e427471e3201b9bfc5a2bf135de8e66fe
Sipherion <sipherion@gmail.com>
2016-12-29 22:32:27 +0100

    Adding translation for ForgotPassword service

--- resources/lang/fr/all.php
+++ resources/lang/fr/all.php
@@ -51,6 +51,23 @@ return [
         'connect'          => 'Se connecter',
         'missing_password' => 'Mot de passe oublié'
     ],
+    'forgot_password'      => [
+        'intro'		       => 'Pour recevoir un nouveau mot de passe, veuillez saisir ci-dessous votre adresse e-mail ; vous recevrez un lien de confirmation sur lequel vous devrez cliquer, puis votre nouveau mot de passe dans un second e-mail.',
+        'submit'    	   => 'Envoyer',
+        'click_here'   	   => 'Demander un nouveau mot de passe',
+        'mail_hello'      => 'Bonjour',
+        'mail1_subject'	   => 'Aion Server : mot de passe oublié',
+        'mail1_part1'	   => 'Si vous avez fait une demande de nouveau mot de passe sur le site Aion Server, merci de cliquer sur ce lien pour recevoir un nouveau mot de passe :',
+        'mail1_part2'	   => "Si vous n'avez pas fait cette demande, nous vous recommandons de contacter un Administrateur et de changer l'adresse mail de votre compte si possible.",
+        'mail1_sent'	   => "Email envoyé",
+        'mail1_error'	   => "Nous sommes désolés, cette adresse e-mail n'existe pas dans notre base de données.",
+        'mail2_subject'	   => 'Aion Server : votre nouveau mot de passe',
+        'mail2_part1'	   => 'Votre nouveau mot de passe est :',
+        'mail2_part2'	   => 'Pensez à le personnaliser lors de votre prochaine connexion à Aion Server.',
+        'mail2_confirm'	   => 'Un email vous été envoyé avec le nouveau mot de passe',
+        'mail2_error'	   => "Nous sommes désolés mais le token n'est pas bon. Veuillez effectuer une nouvelle demande ou contacter un administrateur.",
+		'mail_ending'	   => 'Cordialement, le Staff Aion Server'
+    ],
     'weddings' => [
         'title'     => 'Mariage',
         'sub_title' => 'Ils ont dit OUI !'
