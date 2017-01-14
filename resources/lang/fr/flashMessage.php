9e206d8fa1955767bdedaf5bf4e422602234dbdb
Sipherion <sipherion@gmail.com>
2017-01-14 23:06:40 +0100

    Implements Search function in Shop for players

--- resources/lang/fr/flashMessage.php
+++ resources/lang/fr/flashMessage.php
@@ -8,7 +8,8 @@ return [
         'fail_category_id' => "La catégorie est surement vide ou n'existe pas",
         'empty_cart'       => "Votre panier est vide. Vous ne pouvez pas accéder à cette page",
         'not_shop_points'  => "Vous n'avez pas assez de point boutique sur votre compte",
-        'success'          => "Achat effectué, tapez la commande : .shop IG sur le personnage "
+        'success'          => "Achat effectué, tapez la commande : .shop IG sur le personnage ",
+		'no_search_empty'  => "Veuillez indiquer un élément de recherche"
     ],
     'vote' => [
         'wait_time'            => "Vous ne pouvez pas voter pour ce site, vous devez attendre 2H"
